5e2c9e8ab5120a08086ac00ae63db446e3c7f203
karolinabarnik <karolinabarnik@gmail.com>
2022-04-20 16:00:54 +0200

    Add hamburger menu

---
+++ js/hamburgermenu.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+
+//const navLink = document.querySelectorAll(".nav-trigger");
+
+//navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+//function closeMenu() {
+//    hamburger.classList.remove("active");
+ //   navMenu.classList.remove("active");
+//}
