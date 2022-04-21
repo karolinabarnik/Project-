1e679f823b82d41b598642792b7171d835511539
karolinabarnik <karolinabarnik@gmail.com>
2022-04-21 09:46:44 +0200

    Fixed hamburger menu

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,19 +1,15 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+const navLink = document.querySelectorAll(".nav-trigger");
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
-
-
-
-//const navLink = document.querySelectorAll(".nav-trigger");
-
-//navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-//function closeMenu() {
-//    hamburger.classList.remove("active");
- //   navMenu.classList.remove("active");
-//}
